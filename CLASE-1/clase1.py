6d449c0873093870a2bd2a0704bb00b5faf0a2ba
Martina Garcia <martinagg@myuax.com>
2024-02-06 15:40:06 +0100

    .

---
+++ CLASE-1/clase1.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# Configuración de la raíz
+root = Tk()
+root.title("Hola mundo")
+root.resizable(1,1)
+root.iconbitmap('hola.ico')
+
+# Finalmente bucle de la aplicación
+root.mainloop()
